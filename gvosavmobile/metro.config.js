7080bee7ed829ae558416685488ce5b95bfb83f6
frankgvos <frank@gvos.io>
2021-01-07 12:08:01 +0530

    Updated the metro config file

--- gvosavmobile/metro.config.js
+++ gvosavmobile/metro.config.js
@@ -4,8 +4,16 @@
  *
  * @format
  */
+const blacklist = require('metro-config/src/defaults/blacklist');
 
 module.exports = {
+  resolver: {
+    blacklistRE: blacklist([
+      /\/nodejs-assets\/.*/,
+      /\/android\/.*/,
+      /\/ios\/.*/
+    ])
+  },	
   transformer: {
     getTransformOptions: async () => ({
       transform: {
