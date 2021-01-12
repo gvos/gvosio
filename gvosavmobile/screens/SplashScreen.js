7c7260263ca36b5a22c0b4807a2f300d5ff2d921
JeremyConnor <ultra31magnus@gmail.com>
2021-01-12 16:10:42 +0530

    Updated the SplashScreen

--- gvosavmobile/screens/SplashScreen.js
+++ gvosavmobile/screens/SplashScreen.js
@@ -18,4 +18,58 @@ const SplashScreen = ({navigation}) => {
     console.log("colors : ", colors);
 
     return ();
-}
+};
+
+export default SplashScreen;
+
+const {height} = Dimensions.get("screen");
+const height_logo = height * 0.25;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    backgroundColor: '#242424'
+  },
+  header: {
+      flex: 2,
+      justifyContent: 'center',
+      alignItems: 'center'
+  },
+  footer: {
+      flex: 1,
+      backgroundColor: '#fff',
+      borderTopLeftRadius: 30,
+      borderTopRightRadius: 30,
+      paddingVertical: 50,
+      paddingHorizontal: 30
+  },
+  logo: {
+      width: height_logo,
+      height: height_logo * 0.7
+  },
+  title: {
+      color: '#05375a',
+      fontSize: 30,
+      fontWeight: 'bold'
+  },
+  text: {
+      color: 'grey',
+      marginTop:5
+  },
+  button: {
+      alignItems: 'flex-end',
+      marginTop: 30
+  },
+  signIn: {
+      width: 150,
+      height: 40,
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 50,
+      flexDirection: 'row'
+  },
+  textSign: {
+      color: 'white',
+      fontWeight: 'bold'
+  }
+});
