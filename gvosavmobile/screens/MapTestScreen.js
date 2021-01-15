fb3d70520ed8c3beaa2df15a21ab73ecac191539
frankgvos <frank@gvos.io>
2021-01-15 21:33:19 +0530

    Updating MapTest Screen

--- gvosavmobile/screens/MapTestScreen.js
+++ gvosavmobile/screens/MapTestScreen.js
@@ -29,3 +29,48 @@ const MapTestScreen = () => {
 };
 
 export default MapTestScreen;
+
+const styles = StyleSheet.create({
+  map: {
+    height: '100%'
+  },
+  // Callout bubble
+  bubble: {
+    flexDirection: 'column',
+    alignSelf: 'flex-start',
+    backgroundColor: '#fff',
+    borderRadius: 6,
+    borderColor: '#ccc',
+    borderWidth: 0.5,
+    padding: 15,
+    width: 150,
+  },
+  // Arrow below the bubble
+  arrow: {
+    backgroundColor: 'transparent',
+    borderColor: 'transparent',
+    borderTopColor: '#fff',
+    borderWidth: 16,
+    alignSelf: 'center',
+    marginTop: -32,
+  },
+  arrowBorder: {
+    backgroundColor: 'transparent',
+    borderColor: 'transparent',
+    borderTopColor: '#007a87',
+    borderWidth: 16,
+    alignSelf: 'center',
+    marginTop: -0.5,
+    // marginBottom: -15
+  },
+  // Character name
+  name: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  // Character image
+  image: {
+    width: "100%",
+    height: 80,
+  },
+});
