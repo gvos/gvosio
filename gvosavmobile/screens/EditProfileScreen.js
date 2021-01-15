5635a00683c26d6c68755dacc576e2ee8a0bedf1
JeremyConnor <ultra31magnus@gmail.com>
2021-01-15 21:41:17 +0530

    Adding edit for Profile Screen

---
+++ gvosavmobile/screens/EditProfileScreen.js
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ImageBackground,
+  TextInput,
+  StyleSheet,
+} from 'react-native';
+
+import {useTheme} from 'react-native-paper';
+
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Feather from 'react-native-vector-icons/Feather';
+
+import BottomSheet from 'reanimated-bottom-sheet';
+import Animated from 'react-native-reanimated';
+
+import ImagePicker from 'react-native-image-crop-picker';
+
+const EditProfileScreen = () => {
+	return();
+};
+
+export default EditProfileScreen;
