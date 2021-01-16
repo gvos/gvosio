b83e7aab04275e657d889ffaf5d9495e13dd2e52
JeremyConnor <ultra31magnus@gmail.com>
2021-01-16 21:20:19 +0530

    Updated the SendScreen

--- gvosavmobile/screens/SendScreen.js
+++ gvosavmobile/screens/SendScreen.js
@@ -39,3 +39,54 @@ const SendScreen = () => {
 };
 
 export default SendScreen;
+
+const styles = StyleSheet.create({
+   container: {
+      paddingTop: 23,
+   },
+   input: {
+      margin: 15,
+      height: 40,
+      borderColor: '#7a42f4',
+      borderWidth: 1
+   },
+   sendButton: {
+      backgroundColor: '#7a42f4',
+      borderRadius: 10,
+      padding: 10,
+      margin: 30,
+      height: 40,
+      width: 80,
+      alignItems: 'center',
+   },
+   sendButtonText:{
+      color: 'white'
+   },
+   MainContainer: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+   },
+   TextInputStyle:{
+      textAlign: 'center',
+      height: 60,
+      width: '93%',
+      color: 'white',
+      fontSize: 20
+   },
+    button: {
+        alignItems: 'center',
+        marginTop: 50
+    },
+    signIn: {
+        width: '60%',
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+    },
+    textSign: {
+        fontSize: 18,
+        fontWeight: 'bold'
+    }   
+});
