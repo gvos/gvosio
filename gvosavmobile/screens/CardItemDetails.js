8b91af8a8eb642ac705407bf5a10c70bf25d8fb0
frankgvos <frank@gvos.io>
2021-01-16 21:12:47 +0530

    Updated CardItemDetails

--- gvosavmobile/screens/CardItemDetails.js
+++ gvosavmobile/screens/CardItemDetails.js
@@ -27,3 +27,80 @@ const CardItemDetails = ({route}) => {
 };
 
 export default CardItemDetails;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    height: MAX_HEIGHT,
+    width: Dimensions.get('window').width,
+    alignSelf: 'stretch',
+    resizeMode: 'cover',
+  },
+  title: {
+    fontSize: 20,
+  },
+  name: {
+    fontWeight: 'bold',
+  },
+  section: {
+    padding: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
+    backgroundColor: 'white',
+  },
+  sectionTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  sectionContent: {
+    fontSize: 16,
+    textAlign: 'justify',
+  },
+  categories: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    flexWrap: 'wrap',
+  },
+  categoryContainer: {
+    flexDirection: 'row',
+    backgroundColor: '#FF6347',
+    borderRadius: 20,
+    margin: 10,
+    padding: 10,
+    paddingHorizontal: 15,
+  },
+  category: {
+    fontSize: 14,
+    color: '#fff',
+    marginLeft: 10,
+  },
+  titleContainer: {
+    flex: 1,
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageTitle: {
+    color: 'white',
+    backgroundColor: 'transparent',
+    fontSize: 24,
+  },
+  navTitleView: {
+    height: MIN_HEIGHT,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: Platform.OS === 'ios' ? 40 : 5,
+    opacity: 0,
+  },
+  navTitle: {
+    color: 'white',
+    fontSize: 18,
+    backgroundColor: 'transparent',
+  },
+  sectionLarge: {
+    minHeight: 300,
+  },
+});
