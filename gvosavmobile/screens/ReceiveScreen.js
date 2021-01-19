c735d5213102cf749e80bd0f6745f5458936e8c4
JeremyConnor <ultra31magnus@gmail.com>
2021-01-19 14:10:11 +0530

    Updated Recceive Screen

--- gvosavmobile/screens/ReceiveScreen.js
+++ gvosavmobile/screens/ReceiveScreen.js
@@ -19,8 +19,82 @@ import {userInfo} from './SignInScreen.js';
 import '../global.js';
 import Users from '../model/users';
 
+const localIPaddress;
+
 const ReceiveScreen = () => {
   return();
 };
 
 export default ReceiveScreen;
+
+export default ReceiveScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  sliderContainer: {
+    height: 200,
+    width: '90%',
+    marginTop: 10,
+    justifyContent: 'center',
+    alignSelf: 'center',
+    borderRadius: 8,
+  },
+
+  wrapper: {},
+
+  slide: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'transparent',
+    borderRadius: 8,
+  },
+  sliderImage: {
+    height: '100%',
+    width: '100%',
+    alignSelf: 'center',
+    borderRadius: 8,
+  },
+  backgroundImage:{
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    opacity: 0.9
+  },
+  categoryContainer: {
+    flexDirection: 'row',
+    width: '90%',
+    alignSelf: 'center',
+    marginTop: 25,
+    marginBottom: 10,
+  },
+  categoryBtn: {
+    flex: 1,
+    width: '30%',
+    marginHorizontal: 0,
+    alignSelf: 'center',
+  },
+  categoryIcon: {
+    borderWidth: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignSelf: 'center',
+    width: 70,
+    height: 70,
+    backgroundColor: '#fdeae7' /* '#FF6347' */,
+    borderRadius: 50,
+  },
+  categoryBtnTxt: {
+    alignSelf: 'center',
+    marginTop: 5,
+    color: '#de4f35',
+  },
+  cardsWrapper: {
+    marginTop: 20,
+    width: '90%',
+    alignSelf: 'center',
+  },
+});
