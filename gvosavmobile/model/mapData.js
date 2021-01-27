128d08f60a4566545874456d47317d57cf8d87ce
JeremyConnor <ultra31magnus@gmail.com>
2021-01-27 16:42:15 +0530

    Added map data

---
+++ gvosavmobile/model/mapData.js
@@ -0,0 +1,264 @@
+const Images = [
+    { image: require("../assets/banners/golden-gate.jpg") },
+    { image: require("../assets/banners/lombart-street.jpg") },
+    { image: require("../assets/banners/sunset-district.jpg") },
+    { image: require("../assets/banners/bernal-heights.jpg") },
+    { image: require("../assets/banners/noe-valley.jpg") },
+];
+
+export const markers = [
+    {
+      coordinate: {
+        latitude: 37.819929,
+        longitude: -122.478255,
+      },
+      title: "Golden Gate Bridge",
+      description: "",
+      image: Images[0].image,
+      rating: 5,
+      reviews: 99,
+    },
+    {
+      coordinate: {
+        latitude: 37.801945,
+        longitude: -122.418892,
+      },
+      title: "Lombart Street",
+      description: "",
+      image: Images[1].image,
+      rating: 5,
+      reviews: 102,
+    },
+    {
+      coordinate: {
+        latitude: 37.7535418,
+        longitude: -122.4952495,
+      },
+      title: "Sunset District",
+      description: "",
+      image: Images[2].image,
+      rating: 3,
+      reviews: 220,
+    },
+    {
+      coordinate: {
+        latitude: 37.744385,
+        longitude: -122.417046,
+      },
+      title: "Bernal Heights",
+      description: "",
+      image: Images[3].image,
+      rating: 4,
+      reviews: 48,
+    },
+    {
+      coordinate: {
+        latitude: 37.748552776,
+        longitude: -122.435983658,
+      },
+      title: "Noe Valley",
+      description: "",
+      image: Images[3].image,
+      rating: 4,
+      reviews: 48,
+    },
+];
+
+export const mapDarkStyle = [
+    {
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#212121"
+        }
+      ]
+    },
+    {
+      "elementType": "labels.icon",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#757575"
+        }
+      ]
+    },
+    {
+      "elementType": "labels.text.stroke",
+      "stylers": [
+        {
+          "color": "#212121"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#757575"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative.country",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#9e9e9e"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative.land_parcel",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative.locality",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#bdbdbd"
+        }
+      ]
+    },
+    {
+      "featureType": "poi",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#757575"
+        }
+      ]
+    },
+    {
+      "featureType": "poi.park",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#181818"
+        }
+      ]
+    },
+    {
+      "featureType": "poi.park",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#616161"
+        }
+      ]
+    },
+    {
+      "featureType": "poi.park",
+      "elementType": "labels.text.stroke",
+      "stylers": [
+        {
+          "color": "#1b1b1b"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "geometry.fill",
+      "stylers": [
+        {
+          "color": "#2c2c2c"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#8a8a8a"
+        }
+      ]
+    },
+    {
+      "featureType": "road.arterial",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#373737"
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#3c3c3c"
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway.controlled_access",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#4e4e4e"
+        }
+      ]
+    },
+    {
+      "featureType": "road.local",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#616161"
+        }
+      ]
+    },
+    {
+      "featureType": "transit",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#757575"
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#000000"
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#3d3d3d"
+        }
+      ]
+    }
+  ];
+
+  export const mapStandardStyle = [
+    {
+      "elementType": "labels.icon",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+  ];
+
