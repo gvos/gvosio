c8ad2dcd1781c84c8c565849fae67847bacfdb40
JeremyConnor <ultra31magnus@gmail.com>
2021-01-29 16:30:58 +0530

    Added Notification

---
+++ gvosavmobile/model/Notifications.js
@@ -0,0 +1,22 @@
+export default Notifications = [
+    {
+        id: 1,
+        title: 'Registration Complete',
+        details: 'You have successfully registered yourself.'
+    },
+    {
+        id: 2,
+        title: 'Asset Added',
+        details: 'You are now the owner othis asset.'
+    },
+    {
+        id: 3,
+        title: 'Asset Sent',
+        details: 'You have successfully sent your asset.'
+    },
+    {
+        id: 4,
+        title: 'Balance',
+        details: 'Your balance is 675.'
+    },
+];
