348f19332742459447cca2832267f193a59d21dd
frankgvos <frank@gvos.io>
2021-03-28 16:02:00 +0530

    Added cards

---
+++ gvosavmobile/components/Card.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {View, Text, Image, StyleSheet, TouchableOpacity} from 'react-native';
+
+import StarRating from './StarRating';
+
+const Card = ({itemData, onPress}) => {
+  return ();
+};
