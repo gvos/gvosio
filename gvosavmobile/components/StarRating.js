ebafb411442f8a87b5b76e1de6099fd7bbc95549
frankgvos <frank@gvos.io>
2021-04-03 15:20:13 +0530

    Added new filw

---
+++ gvosavmobile/components/StarRating.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const StarRating = (props) => {
+}
+
+export default StarRating;
