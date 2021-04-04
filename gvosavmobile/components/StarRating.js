9b41b0b15d698252c7e4b4fd23d7115fa34f6f4d
frankgvos <frank@gvos.io>
2021-04-04 15:44:00 +0530

    Updating new file

--- gvosavmobile/components/StarRating.js
+++ gvosavmobile/components/StarRating.js
@@ -4,6 +4,16 @@ import { StyleSheet, View, Text } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const StarRating = (props) => {
+
+    // This array will contain our star tags. We will include this
+    // array between the view tag.
+    let stars = [];
+    // Loop 5 times
+    for (var i = 1; i <= 5; i++) {
+        // set the path to filled stars
+        let name = 'ios-star';
+        // If ratings is lower, set the path to unfilled stars
+
 }
 
 export default StarRating;
