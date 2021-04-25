01c4a80e9f085772aa4f1b71d92cea8b7babbb72
JeremyConnor <ultra31magnus@gmail.com>
2021-04-25 16:51:23 +0530

    updated bookomark

--- gvosavmobile/screens/BookmarkScreen.js
+++ gvosavmobile/screens/BookmarkScreen.js
@@ -225,4 +225,9 @@ const styles = StyleSheet.create({
     left: 80,
     alignItems: 'center'
   },
+  parent: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
 });
